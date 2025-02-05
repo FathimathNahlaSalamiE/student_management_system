c31416116817004e746a6890475552153324133f
Fathimath Nahla Salami E <nahlasalami321@gmail.com>
2025-02-05 07:44:07 +0530

    customize user model

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -24,4 +24,5 @@ from .import views,Hod_Views,Staff_Views,Student_Views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/',views.BASE,name="base"),
+    path('login/',views.LOGIN,name="login"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
