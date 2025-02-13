728e6cd1cea7eb2e8ae993578129086ac2634cb1
Fathimath Nahla Salami E <nahlasalami321@gmail.com>
2025-02-13 21:59:06 +0530

    CRUD of course

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -36,11 +36,18 @@ urlpatterns = [
 
     # for hod
     path('Hod/Home',Hod_Views.HOME,name="hod_home"),
+
     path('Hod/Student/Add',Hod_Views.ADD_STUDENT,name="add_student"),
     path('Hod/Student/View',Hod_Views.VIEW_STUDENT,name="view_student"),
     path('Hod/Student/Edit/<str:id>',Hod_Views.EDIT_STUDENT,name="edit_student"),
     path('Hod/Student/Update',Hod_Views.UPDATE_STUDENT,name="update_student"),
     path('Hod/Student/Delete/<str:admin>',Hod_Views.DELETE_STUDENT,name="delete_student"),
+
+    path('Hod/Course/Add',Hod_Views.ADD_COURSE,name="add_course"),
+    path('Hod/Course/View',Hod_Views.VIEW_COURSE,name="view_course"),
+    path('Hod/Course/Edit/<str:id>',Hod_Views.EDIT_COURSE,name="edit_course"),
+    path('Hod/Course/Update',Hod_Views.UPDATE_COURSE,name="update_course"),
+    path('Hod/Course/Delete/<str:id>',Hod_Views.DELETE_COURSE,name="delete_course"),
 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
